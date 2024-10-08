3a15b61e664248459a36c755818b310ce57a41ec
phatlenguyen166 <phatlenguyen166@gmail.com>
2024-10-08 17:38:18 +0700

    create class contaainer tailwind

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,6 +1,10 @@
+const plugin = require('tailwindcss/plugin')
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  corePlugins: {
+    container: false
+  },
   theme: {
     extend: {
       colors: {
@@ -8,5 +12,17 @@ module.exports = {
       }
     }
   },
-  plugins: []
+  plugins: [
+    plugin(function ({ addComponents }) {
+      addComponents({
+        '.container': {
+          maxWidth: '80rem',
+          marginLeft: 'auto',
+          marginRight: 'auto',
+          paddingLeft: '1rem',
+          paddingRight: '1rem'
+        }
+      })
+    })
+  ]
 }
