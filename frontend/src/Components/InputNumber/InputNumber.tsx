6a005badcce1481f6a00acf4ce31c5b8d659b345
phatlenguyen166 <phatlenguyen166@gmail.com>
2024-11-14 22:36:27 +0700

    fix InputNumber and QuantityController

--- frontend/src/Components/InputNumber/InputNumber.tsx
+++ frontend/src/Components/InputNumber/InputNumber.tsx
@@ -1,4 +1,4 @@
-import { forwardRef, InputHTMLAttributes } from 'react'
+import { forwardRef, InputHTMLAttributes, useState } from 'react'
 import { RegisterOptions, UseFormRegister } from 'react-hook-form'
 
 export interface InputNumberProps extends InputHTMLAttributes<HTMLInputElement> {
@@ -13,21 +13,26 @@ const InputNumber = forwardRef<HTMLInputElement, InputNumberProps>(function Inpu
     className,
     classNameInput = 'w-full rounded-sm border border-gray-300 p-3 outline-none focus:border-gray-500 focus:shadow-sm',
     classNameError = 'mt-1 min-h-[1.25rem] pl-1 text-sm text-red-500',
+    value = '',
     onChange,
     ...rest
   },
   ref
 ) {
+  const [localValue, setLocalValue] = useState<string>(value as string)
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const { value } = event.target
-    if ((/^\d+$/.test(value) || value === '') && onChange) {
-      onChange(event)
+    if (/^\d+$/.test(value) || value === '') {
+      if (onChange) {
+        onChange(event)
+      }
+      setLocalValue(value)
     }
   }
 
   return (
     <div className={className}>
-      <input ref={ref} className={classNameInput} {...rest} onChange={handleChange} />
+      <input ref={ref} className={classNameInput} onChange={handleChange} value={value || localValue} {...rest} />
       {errorMessage && <div className={classNameError}>{errorMessage}</div>}
     </div>
   )
